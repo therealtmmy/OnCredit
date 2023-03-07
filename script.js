c535d7a276e3a8cc745183d6c209f0a4f81ced5d
Oluwatimilehin Esan <107879861+therealtmmy@users.noreply.github.com>
2023-03-07 11:50:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Documentation
+// Fetch() method: It is defined on the window object, which we can use to perform request.
+// This method will return promise.
+// Promise will either be fullfilled or rejected.
+
+// To be fullfilled which means to display, you use return to reject, you use catch.
+
+// we use return data.json() to return an api to objects
+
+fetch('https://fakestoreapi.com/products').then((data)=>{
+    // console.log(data)
+    return data.json(); 
+}).then((completedata)=>{
+    // console.log(completedata);
+    let data1 = "";
+    completedata.map((values)=>{
+        data1 +=`  <div class="card">
+        <h1 class="title">${values.title}</h1>
+        <img src="${values.image}" alt="img">
+        <p>${values.description}</p>
+        <h3 class="category">${values.category}</h3>
+        <h3 class="price">${values.price}</h3>
+    </div>`
+    });
+    
+    document.querySelector(".cards").innerHTML = data1
+})
