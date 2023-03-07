054a658572ce7792d141840c2990dd9371e590f0
Oluwatimilehin Esan <107879861+therealtmmy@users.noreply.github.com>
2023-03-08 00:21:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,13 +7,13 @@
 
 // we use return data.json() to return an api to objects
 
-fetch('https://fakestoreapi.com/products').then((data)=>{
+fetch('https://fakestoreapi.com/products').then(data=>{
     // console.log(data)
     return data.json(); 
-}).then((completedata)=>{
-    // console.log(completedata);
+}).then(completedata =>{
+    console.log(completedata);
     let data1 = "";
-    completedata.map((values)=>{
+    completedata.forEach((values)=>{
         data1 +=`  <div class="card">
         <h1 class="title">${values.title}</h1>
         <img src="${values.image}" alt="img">
