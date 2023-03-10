b336cdf1e1c2d813bfff8873955d063dcf7c97fc
Oluwatimilehin Esan <107879861+therealtmmy@users.noreply.github.com>
2023-03-10 03:33:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,17 +6,19 @@
 // To be fullfilled which means to display, you use return to reject, you use catch.
 
 // we use return data.json() to return an api to objects
+// Another API URL Used www.fakeapistore.com
 
-fetch('https://fakestoreapi.com/products').then(data=>{
+
+fetch('https://dummyjson.com/products').then(data=>{
     // console.log(data)
     return data.json(); 
 }).then(completedata =>{
-    console.log(completedata);
+    console.log(completedata.products);
     let data1 = "";
-    completedata.forEach((values)=>{
+    completedata.products.map((values)=>{
         data1 +=`  <div class="card">
         <h1 class="title">${values.title}</h1>
-        <img src="${values.image}" alt="img">
+        <img src="${values.thumbnail}" alt="img">
         <p>${values.description}</p>
         <h3 class="category">${values.category}</h3>
         <h3 class="price">${values.price}</h3>
